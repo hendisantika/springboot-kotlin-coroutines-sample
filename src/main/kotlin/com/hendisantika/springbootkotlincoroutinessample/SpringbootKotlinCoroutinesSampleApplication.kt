3c11af34b701e5c52306d6c6373b6bf1670305f3
Hendi Santika <hendisantika@gmail.com>
2021-05-23 06:22:54 +0700

    #1 - Initial Commit

---
+++ src/main/kotlin/com/hendisantika/springbootkotlincoroutinessample/SpringbootKotlinCoroutinesSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.hendisantika.springbootkotlincoroutinessample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootKotlinCoroutinesSampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootKotlinCoroutinesSampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/hendisantika/springbootkotlincoroutinessample/SpringbootKotlinCoroutinesSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootkotlincoroutinessample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootKotlinCoroutinesSampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
