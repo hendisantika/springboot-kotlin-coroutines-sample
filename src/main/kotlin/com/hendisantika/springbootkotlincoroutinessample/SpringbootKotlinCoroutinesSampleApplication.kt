f66eb43422d4da2bddb7c2c533958d2dec880b9e
Hendi Santika <hendisantika@gmail.com>
2021-05-23 06:28:59 +0700

    #4 - Add routes Beans

--- src/main/kotlin/com/hendisantika/springbootkotlincoroutinessample/SpringbootKotlinCoroutinesSampleApplication.kt
+++ src/main/kotlin/com/hendisantika/springbootkotlincoroutinessample/SpringbootKotlinCoroutinesSampleApplication.kt
@@ -2,9 +2,20 @@ package com.hendisantika.springbootkotlincoroutinessample
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class SpringbootKotlinCoroutinesSampleApplication
+class SpringbootKotlinCoroutinesSampleApplication {
+    @Bean
+    fun routes(handlers: Handlers) = coRouter {
+        GET("/", handlers::index)
+        GET("/suspend", handlers::suspending)
+        GET("/sequential-flow", handlers::sequentialFlow)
+        GET("/concurrent-flow", handlers::concurrentFlow)
+        GET("/error", handlers::error)
+    }
+}
+
 
 fun main(args: Array<String>) {
     runApplication<SpringbootKotlinCoroutinesSampleApplication>(*args)
